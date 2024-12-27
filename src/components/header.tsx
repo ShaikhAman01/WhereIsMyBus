25bc4212792d496fac42d0b65de4ab72d40a16b6
Shaikh Aman <110104595+ShaikhAman01@users.noreply.github.com>
2024-12-27 20:02:06 +0530

    add sticky

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Vector from '../assets/adjustments.svg';
 
 const Header = () => {
   return (
-    <header className="bg-white px-4 py-2 flex items-center justify-between sticky">
+    <header className="bg-white px-4 py-2 flex items-center justify-between sticky top-0 z-10 shadow-md">
 <img src={AppLogo} alt="App Logo" className="w-8 h-8" />
 <img src={Vector} alt="App Logo" className="w-8 h-8" />
     </header>
